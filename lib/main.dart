7b79788477c032487a05eb60c82ebcb7561994d8
Khulow <irvinsenwedi@gmail.com>
2024-04-05 13:19:48 +0200

    n

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
 
+<<<<<<< Updated upstreamgit;
 void main() {
   runApp(const MainApp());
+=======
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    //options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MainApp()); 
+>>>>>>> Stashed changesc
 }
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
-  @override
+  @overrideyw
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
